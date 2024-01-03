e8319cec0fe8b3911bec8fbad435154752efa51b
Andri Noviandy <andrinoviandy88@gmail.com>
2024-01-03 15:10:27 +0700

    tsting

--- config/koneksi.php
+++ config/koneksi.php
@@ -5,7 +5,7 @@
 $hostname_koneksi = "localhost";
 $database_koneksi = "db_kharisma_new";
 $username_koneksi = "root";
-$password_koneksi = "";
+$password_koneksi = "test";
 $koneksi = new mysqli($hostname_koneksi, $username_koneksi, $password_koneksi, $database_koneksi); 
 
 
