91e051ca9ad5b4a3ea62f01621359cdd673fcfe1
Andri Noviandy <andrinoviandy88@gmail.com>
2024-02-28 14:10:52 +0700

    28-02-2024

--- data/modal-pembeli.php
+++ data/modal-pembeli.php
@@ -3,11 +3,17 @@ include("../config/koneksi.php");
 include("../include/API.php");
 session_start();
 error_reporting(0);
-$data = mysqli_fetch_array(mysqli_query($koneksi, "select nama_pembeli, jalan, kontak_rs from pembeli where id = ".$_GET['id'].""))
+$data = mysqli_fetch_array(mysqli_query($koneksi, "select nama_pembeli, jalan, kontak_rs, nama_provinsi, nama_kabupaten, nama_kecamatan from pembeli, alamat_provinsi, alamat_kabupaten, alamat_kecamatan where alamat_provinsi.id = pembeli.provinsi_id and alamat_kabupaten.id=pembeli.kabupaten_id and alamat_kecamatan.id = pembeli.kecamatan_id and pembeli.id = ".$_GET['id'].""))
 ?>
 <p align="justify">
     <?php
     echo "<b>Nama RS/Dinas/Klinik/Dll :</b> <br/>" . $data['nama_pembeli']; ?>
+    <hr />
+    <?php echo "<b>Provinsi :</b> <br/>" . ucwords(strtolower($data['nama_provinsi'])); ?>
+    <hr />
+    <?php echo "<b>Kabupaten :</b> <br/>" . ucwords(strtolower($data['nama_kabupaten'])); ?>
+    <hr />
+    <?php echo "<b>Kecamatan :</b> <br/>" . ucwords(strtolower($data['nama_kecamatan'])); ?>
     <hr />
     <?php echo "<b>Alamat :</b> <br/>" . str_replace("<br>", "", $data['jalan']); ?>
     <hr />
