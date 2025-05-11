7538c933c8c460bff321d148b8805ed32e21a1e1
Andri Noviandy <andrinoviandy88@gmail.com>
2025-05-11 12:55:17 +0700

    riwayat buku kas

--- json/riwayat_hutang_piutang.php
+++ json/riwayat_hutang_piutang.php
@@ -10,10 +10,10 @@ if (isset($_GET['tgl1']) and isset($_GET['tgl2'])) {
 $sql = "select *,utang_piutang.nominal as nominal_up,utang_piutang.id as id_up from utang_piutang_bayar,buku_kas,utang_piutang where utang_piutang.id=utang_piutang_bayar.utang_piutang_id and buku_kas.id=utang_piutang_bayar.buku_kas_id and tgl_bayar between '$_GET[tgl1]' and '$_GET[tgl2]' group by utang_piutang_id order by utang_piutang.tgl_input DESC, utang_piutang.id DESC";
 }
 elseif (isset($_GET['kunci']) and isset($_GET['pilihan'])) {
-$sql = "select *,utang_piutang.nominal as nominal_up,utang_piutang.id as id_up from utang_piutang_bayar,buku_kas,utang_piutang where utang_piutang.id=utang_piutang_bayar.utang_piutang_id and buku_kas.id=utang_piutang_bayar.buku_kas_id and buku_kas.id=$_GET[id] and $_GET[pilihan] like '%$_GET[kunci]%' group by utang_piutang_id order by utang_piutang.tgl_input DESC, utang_piutang.id DESC";
+$sql = "select *,utang_piutang.nominal as nominal_up,utang_piutang.id as id_up from utang_piutang_bayar,buku_kas,utang_piutang where utang_piutang.id=utang_piutang_bayar.utang_piutang_id and buku_kas.id=utang_piutang_bayar.buku_kas_id and buku_kas.id='$_GET[id]' and $_GET[pilihan] like '%$_GET[kunci]%' group by utang_piutang_id order by utang_piutang.tgl_input DESC, utang_piutang.id DESC";
 }
  else {
-$sql = "select *,utang_piutang.nominal as nominal_up,utang_piutang.id as id_up from utang_piutang_bayar,buku_kas,utang_piutang where utang_piutang.id=utang_piutang_bayar.utang_piutang_id and buku_kas.id=utang_piutang_bayar.buku_kas_id and buku_kas.id=$_GET[id] group by utang_piutang_id order by utang_piutang.tgl_input DESC, utang_piutang.id DESC LIMIT 100";
+$sql = "select *,utang_piutang.nominal as nominal_up,utang_piutang.id as id_up from utang_piutang_bayar,buku_kas,utang_piutang where utang_piutang.id=utang_piutang_bayar.utang_piutang_id and buku_kas.id=utang_piutang_bayar.buku_kas_id and buku_kas.id='$_GET[id]' group by utang_piutang_id order by utang_piutang.tgl_input DESC, utang_piutang.id DESC LIMIT 100";
 }
 $result = mysqli_query($koneksi, $sql) or die("Error " . mysqli_error($koneksi));
  
